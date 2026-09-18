e0dc41e4cc5f1be45748eac9304e467bef09e49c
Peter Bachant <pwbachant@gmail.com>
2026-09-18 15:29:30 +0000

    Add stage plot-penguins to produce figures/penguins.png

---
+++ scripts/plot-penguins.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("data/raw/penguins.csv")
+
+fig, ax = plt.subplots(figsize=(5, 3.5))
+ax.plot(df["bill_length_mm"], df["bill_depth_mm"], "o")
+ax.set_xlabel("bill_length_mm")
+ax.set_ylabel("bill_depth_mm")
+fig.tight_layout()
+fig.savefig("figures/penguins.png", dpi=150)
