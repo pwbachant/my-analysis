e1d1257a8cf28bf82b6308e85b5b838ca83bec94
Peter Bachant <pwbachant@gmail.com>
2026-09-19 16:49:42 +0000

    Update stage plot-penguins producing figures/penguins.png

--- scripts/plot-penguins.py
+++ scripts/plot-penguins.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.read_csv("data/raw/penguins.csv")
 
 fig, ax = plt.subplots(figsize=(5, 3.5))
-ax.plot(df["bill_length_mm"], df["bill_depth_mm"], "o")
+ax.plot(df["bill_length_mm"], df["bill_depth_mm"], "o", color="g")
 ax.set_xlabel("bill_length_mm")
 ax.set_ylabel("bill_depth_mm")
 fig.tight_layout()
